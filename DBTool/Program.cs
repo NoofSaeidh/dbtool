4b27ed95cf85fd7890fb6371f8650dc120ad4a41
Alexey Kuznetsov <lehasmith@yandex.ru>
2017-04-22 01:07:07 +0300

    #if debug

--- DBTool/Program.cs
+++ DBTool/Program.cs
@@ -9,10 +9,17 @@ namespace DBTool
 {
     class Program
     {
+#if DEBUG
         static void Main(string[] args)
         {
-            while (true) 
+            while (true)
                 new Interface(Console.ReadLine().Split(' '));
         }
+#else
+        static void Main(string[] args)
+        {
+            new Interface(args);
+        }
+#endif
     }
 }
