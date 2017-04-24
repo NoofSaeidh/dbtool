4346a4557a8af753b10b1171eed76bc44945335c
Alexey Kuznetsov <lehasmith@yandex.ru>
2017-04-24 22:16:42 +0300

    Basical interface functions realized

--- DBTool/CLI/Interface.cs
+++ DBTool/CLI/Interface.cs
@@ -7,6 +7,7 @@ using CommandLine;
 using CommandLine.Text;
 using DBTool.CLI.Options;
 using DBTool.CLI.Configuration;
+using DBTool.Core;
 using System.IO;
 
 namespace DBTool.CLI
@@ -18,8 +19,21 @@ namespace DBTool.CLI
             var result = Parser.Default.ParseArguments<CreateBackup, RestoreBackup, Dialog>(args);
             var mapped = result.MapResult(option => option,_ => null);
             if (mapped is Dialog) throw new NotImplementedException();
-            //(mapped as CreateBackup).
             Config.Initialize(Path.Combine(Environment.CurrentDirectory, "CLI", "Configuration", "Config.xml"));
+
+            var backup = mapped as IBackup;
+            if(backup!=null)
+            {
+                if (backup.Server == null) backup.Server = Config.Instance.DefaultServer;
+
+                using (var database = new Database(backup.Server, backup.Database))
+                {
+                    if (backup is CreateBackup)
+                        database.CreateBackup(backup.Path);
+                    else// if(backup is RestoreBackup)
+                        database.RestoreBackup(backup.Path);
+                }
+            }
         }
     }
 }
